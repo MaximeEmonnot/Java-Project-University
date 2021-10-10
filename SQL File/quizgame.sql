39718dcf7d8228f7467e1b39b54254829d95b459
DialloAhmad <ahmad7991tad@gmail.com>
2021-10-10 21:05:20 +0200

    Création de table
    Création des tables utilisateur, professeur et statistiques

--- SQL File/quizgame.sql
+++ SQL File/quizgame.sql
@@ -2,7 +2,7 @@ create database `quizgame`;
 
 /* Création de la table utilisateur*/
 
-Drop table if exists `utilisateur`;
+
 create table `utilisateur`(
 	`id_utilisateur` int not null auto_increment,
     `nom` varchar(30) not null,
@@ -16,7 +16,7 @@ create table `utilisateur`(
 
 /* Création de la table statistique */
 
-Drop table if exists `statistique`;
+
 create table `statistique`(
 	`id_statistique` int not null,
     `score` int,
@@ -24,3 +24,19 @@ create table `statistique`(
     `domaineDeQuiz` varchar(30),
 	constraint fk_stats_utilisateur foreign key (id_statistique) references utilisateur (id_utilisateur) 
 );
+
+/* Création de la table professeur */
+
+
+create table `professeur`(
+	`id_professeur` int not null auto_increment,
+    `nom` varchar(30) not null,
+    `prenom` varchar(50) not null,
+    `email` varchar(50) unique not null,
+    `telephone` varchar(10) unique not null,
+    `motDePass` varchar(20) not null,
+    `specialite` varchar(30) not null,
+    `adresse` varchar(60),
+    constraint pk_professeur primary key (id_professeur)
+);
+
