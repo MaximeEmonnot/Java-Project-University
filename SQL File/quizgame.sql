d2eb3a2dc47e71644116e33fdc8e82b2dcab9c36
DialloAhmad <ahmad7991tad@gmail.com>
2021-10-10 20:00:51 +0200

    Ajout de quizgame sql
    Ajouter d'un dossier "SQL File" qui contient fichier "quizgame.sql" dans c'est dans ce fichier que vous allez mettre le code création de vos tables

---
+++ SQL File/quizgame.sql
@@ -0,0 +1,26 @@
+create database `quizgame`;
+
+/* Création de la table utilisateur*/
+
+Drop table if exists `utilisateur`;
+create table `utilisateur`(
+	`id_utilisateur` int not null auto_increment,
+    `nom` varchar(30) not null,
+    `prenom` varchar(50) not null,
+    `email` varchar(50) unique not null,
+    `telephone` varchar(10) unique not null,
+    `motDePass` varchar(20) not null,
+    `adresse` varchar(60),
+    constraint pk_utilisateur primary key (id_utilisateur)
+);
+
+/* Création de la table statistique */
+
+Drop table if exists `statistique`;
+create table `statistique`(
+	`id_statistique` int not null,
+    `score` int,
+    `PourcentageDeProgression` float,
+    `domaineDeQuiz` varchar(30),
+	constraint fk_stats_utilisateur foreign key (id_statistique) references utilisateur (id_utilisateur) 
+);
