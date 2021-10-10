807829dbbb9a88fbdda0cf677e78a35e1d59e510
DialloAhmad <ahmad7991tad@gmail.com>
2021-10-10 21:09:36 +0200

    Ajout de la table professeur

--- SQL File/quizgame.sql
+++ SQL File/quizgame.sql
@@ -2,7 +2,7 @@ create database `quizgame`;
 
 /* Création de la table utilisateur*/
 
-
+Drop table if exists `utilisateur`;
 create table `utilisateur`(
 	`id_utilisateur` int not null auto_increment,
     `nom` varchar(30) not null,
@@ -16,7 +16,7 @@ create table `utilisateur`(
 
 /* Création de la table statistique */
 
-
+Drop table if exists `statistique`;
 create table `statistique`(
 	`id_statistique` int not null,
     `score` int,
@@ -25,10 +25,9 @@ create table `statistique`(
 	constraint fk_stats_utilisateur foreign key (id_statistique) references utilisateur (id_utilisateur) 
 );
 
-
 /* Création de la table professeur */
 
-
+Drop table if exists `professeur`;
 create table `professeur`(
 	`id_professeur` int not null auto_increment,
     `nom` varchar(30) not null,
@@ -39,37 +38,5 @@ create table `professeur`(
     `specialite` varchar(30) not null,
     `adresse` varchar(60),
     constraint pk_professeur primary key (id_professeur)
-);
-
-
-/*Creation de la table questions */
-
-DROP TABLE IF EXISTS `questions`;
-create table questions(
-	id int auto_increment primary key,
-    code_question varchar(20) unique,
-    id_prof varchar(50) not null,
-    domaine varchar(50) not null,
-    categorie varchar(50) not null,
-    niveau varchar(50) not null,
-    difficulte varchar(50) not null,
-    question varchar(150) not null,
-    code_reponses varchar(20) not null,
-    reponseA varchar(150) not null,
-    reponseB varchar(150) not null,
-    reponseC varchar(150) not null,
-    reponseD varchar(150) not null
-);
-
-/*Creation de la table administrateur*/
-
-DROP TABLE IF EXISTS `admin`;
-create table admin(
-	id int auto_increment primary key,
-    nom varchar(30),
-    prenom varchar(30),
-    tel varchar(20) unique,
-    mail varchar(20) unique,
-    passwd varchar(150)
 );
 
