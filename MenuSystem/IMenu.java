7b88618de4bfa9423d397e0f6f3cbb0527be55d8
Eraindaer <maxime.emonnot41@gmail.com>
2021-12-06 16:11:18 +0100

    Suppressions de classes inutilisées - BasicMenu - IMenu - MenuDecorator - MenuTest

--- MenuSystem/IMenu.java
+++
@@ -1,9 +0,0 @@
-package MenuSystem;
-
-import Exceptions.ProjectException;
-
-public interface IMenu {
-    public abstract void Update();
-    public abstract void Update(CoreSystem.Mouse.EventType e);
-    public abstract void Draw() throws ProjectException;
-}
